14be679ce7018027609a9486749ee18408f1495f
Elfo <renanmedeirosaguiar@gmail.com>
2025-07-19 09:28:51 -0300

    Changed way that note array is generated

--- src/note.rs
+++ src/note.rs
@@ -1,23 +1,38 @@
 use std::default::Default;
 
-const A_INDEX: usize = 53;
 const A_FREQUENCY: f64 = 440_f64;
+const LOWER_FREQUENCY_THRESHOLD: f64 = 20_f64;
+const UPPER_FREQUENCY_THRESHOLD: f64 = 20_000_f64;
+const LOWEST_NOTE: f64 = get_first_note(LOWER_FREQUENCY_THRESHOLD);
 const SEMI_TONE_FACTOR: f64 = f64::from_bits(4607450216769616227);
-const NUMBER_OF_AVAILABLE_NOTES: usize = 120;
+const NUMBER_OF_AVAILABLE_NOTES: usize = get_size(UPPER_FREQUENCY_THRESHOLD);
 pub const AVAILABLE_NOTES: [f64; NUMBER_OF_AVAILABLE_NOTES] = generate_array();
+
+const fn get_first_note(lower_frequency_threshold: f64) -> f64 {
+    let mut frequency: f64 = A_FREQUENCY;
+    while frequency > lower_frequency_threshold {
+        frequency /= SEMI_TONE_FACTOR;
+    }
+    return frequency * SEMI_TONE_FACTOR;
+}
+
+const fn get_size(upper_frequency_threshold: f64) -> usize {
+    let mut frequency: f64 = LOWEST_NOTE;
+    let mut counter = 0;
+    while frequency < upper_frequency_threshold {
+        frequency *= SEMI_TONE_FACTOR;
+        counter += 1;
+    }
+    return counter;
+}
 
 const fn generate_array() -> [f64; NUMBER_OF_AVAILABLE_NOTES] {
     let mut array = [0_f64; NUMBER_OF_AVAILABLE_NOTES];
-    array[A_INDEX] = A_FREQUENCY;
-    let mut index: usize = A_INDEX;
-    while index > 0 {
-        array[index - 1] = array[index] / SEMI_TONE_FACTOR;
-        index -= 1;
-    }
-    index = A_INDEX;
+    array[0] = LOWEST_NOTE;
+    let mut index: usize = 0;
     while index < NUMBER_OF_AVAILABLE_NOTES - 1 {
-        array[index + 1] = array[index] * SEMI_TONE_FACTOR;
         index += 1;
+        array[index] = array[index - 1] * SEMI_TONE_FACTOR;
     }
     return array;
 }
